a542af5ca5efce45e9d414e8d9091d3fea57f875
Ivan Veselov <veselov@gmail.com>
2016-04-10 13:22:06 +0100

    Added show function

--- first.py
+++ first.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2
 
+def show(img):
+    cv2.imshow("", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 img = cv2.imread("images/lena.jpg")
 
 # Create a black image
@@ -11,8 +16,4 @@ img = cv2.imread("images/lena.jpg")
 # Draw a diagonal blue line with thickness of 5 px
 # cv2.line(img, (0,0), (511,511), (255,0,0), 5)
 
-# show picture
-cv2.imshow("Result", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+show(img)
